4d6bad57ae5866de0cf62e1ded86de2f95ac862d
sgodoys10 <sgodoys10@gmail.com>
2024-08-26 12:32:32 -0700

    Img path fixed

--- src/Views/Hero/Hero.jsx
+++ src/Views/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css'
-import heroImg from '../../assets/samuAI2.png'
+import heroImg from '../../assets/SamuAI2.png'
 import sun from '../../assets/sun.svg'
 import moon from '../../assets/moon.svg'
 import linkedinLight from '../../assets/linkedin-light.svg'
